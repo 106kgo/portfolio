b5eb0b8e3f9dd638d1d9495746c26145ae3b22f8
Gabriel Olmos <gabe_olmos@yahoo.com>
2019-03-15 19:11:20 -0700

    configuring old repo to new portfolio site

---
+++ js/javascript.js
@@ -0,0 +1,15 @@
+let cvs = document.getElementById('canvas');
+let ctx = cvs.getContext('2d');
+
+let imageName = new Image();
+imageName.src = "images/img.png";
+
+let audioName = newAudio();
+
+ctx.drawImage(imageName, x, y, width, height);
+ctx.drawImage()
+
+function draw(){
+    ctx.drawImage( bg, 0, 0);
+    ctx.drawImage( pipeNorth, pX, pY);
+}
